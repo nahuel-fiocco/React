b2e7eda61a8ce8197bd9aa943c6fb8797bfc2f04
nahuel-fiocco <nahuelfiocco@gmail.com>
2023-05-25 21:00:08 -0300

    NavBar + Cart

--- nahuel/src/components/NavBar.jsx
+++ nahuel/src/components/NavBar.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import Cart from './Cart'
+import './NavBar.css'
+import samsung from '../assets/samsung-icon-white.png';
 
 function NavBar() {
-  return (
-    <div>
-        <h1>NavBar</h1>
-        <Cart />
-    </div>
-  )
+    return (
+        <div className='container-fluid'>
+            <a className="navbar-brand" href="#">
+                <img width={110} src={samsung} alt="Samsung Icon" />
+            </a>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <a className="nav-link" aria-current="page" href="#">
+                    Celulares
+                </a>
+                <a className="nav-link" aria-current="page" href="#">
+                    Notebooks
+                </a>
+                <a className="nav-link" aria-current="page" href="#">
+                    Soporte
+                </a>
+            </nav>
+            <a className='nav-link' aria-current='page' href='#'>
+                <Cart />
+            </a>
+        </div>
+    )
 }
 
 export default NavBar
