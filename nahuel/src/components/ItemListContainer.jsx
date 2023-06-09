11c131da5c053de87a647e29546f7e00332876b0
nahuel-fiocco <nahuelfiocco@gmail.com>
2023-06-09 16:22:59 -0300

    agregue setTimeout para simular un delay externo y el mensaje de cargando...

--- nahuel/src/components/ItemListContainer.jsx
+++ nahuel/src/components/ItemListContainer.jsx
@@ -2,22 +2,53 @@ import React, { useEffect, useState } from 'react'
 import productos from './Productos.js'
 import './ItemListContainer.css'
 import ItemList from './ItemList.jsx'
+import { BounceLoader } from 'react-spinners'
 import { useParams } from 'react-router-dom'
 
 function ItemListContainer() {
 
   const [productList, setProductList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
   const { categoryId } = useParams()
 
+  const getList = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(productos)
+        // reject(new Error('No se pudo traer la lista de productos'))
+      }, 1000)
+    })
+  }
+
   useEffect(() => {
-    const filteredProducts = categoryId
-      ? productos.filter((producto) => producto.categoria === categoryId) : productos;
-    setProductList(filteredProducts)
+    setLoading(true)
+    getList()
+      .then((result) => {
+        const filteredProducts = categoryId
+          ? result.filter((product) => product.categoria === categoryId) : result;
+        setProductList(filteredProducts)
+      })
+      .catch((error) => {
+        setError(error)
+        console.log(error)
+      })
+      .finally(() => {
+        setLoading(false)
+        console.log('Finalizó la petición')
+      })
   }, [categoryId])
 
   return (
     <div className='contenedor-home'>
-      <ItemList products={productList} />
+      {loading && (
+        <div className="spinner">
+          <BounceLoader color={'#032cfc'} loading={true} size={50} />
+          <p>Loading</p>
+        </div>
+      )}
+      {error && <p>Hubo un error: {error.message}</p>}
+      {!loading && !error && <ItemList products={productList} />}
     </div>
   )
 }
